a449d417bca9678a70aa8c40746dda28ec4e62d9
Walter Leibbrandt <git_wrl_co_za>
2014-10-20 03:01:39 +0200

    Added test for registration of methods

--- tests/test_pyac.py
+++ tests/test_pyac.py
@@ -29,6 +29,20 @@ class TestPyac(unittest.TestCase):
         self.assertIn(show, ACL.managed_funcs)
         self.assertIn(edit, ACL.managed_funcs)
 
+    def test_pyac_registers_methods(self):
+        class WikiPage(object):
+
+            @accesscontrol(lambda user: True)
+            def show():
+                print('Shows the wiki page')
+
+            @accesscontrol(lambda user: user.is_admin)
+            def edit():
+                print('Edit the wiki page')
+
+        self.assertIn(WikiPage.__dict__['show'], ACL.managed_funcs)
+        self.assertIn(WikiPage.__dict__['edit'], ACL.managed_funcs)
+
 
 if __name__ == '__main__':
     unittest.main()
